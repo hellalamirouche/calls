4648126aea0ec2e17eb7922a17f774d331f53640
HELLAL <41439010+hellalamirouche@users.noreply.github.com>
2024-11-02 18:19:45 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://servcall.onrender.com/");// Use your deployed signaling server URL
+var socket = io("https://servcall.onrender.com/");// Use your deployed signaling server URL
 export default socket;
