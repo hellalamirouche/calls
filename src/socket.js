153ab11447d08b8b001f5a3845b35fda03797a9d
HELLAL <41439010+hellalamirouche@users.noreply.github.com>
2024-11-02 17:50:35 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://calls-iota.vercel.app
-"); // Use your deployed signaling server URL
+const socket = io("https://calls-2sw9.onrender.com"); // Use your deployed signaling server URL
 export default socket;
